82cdb22f030d2288393d32598cac6de4160c804a
Yihui0729 <el22174@bristol.ac.uk>
2023-02-21 13:44:05 +0000

    add a file

---
+++ CW1.py
@@ -0,0 +1,186 @@
+#Grids 1-4 are 2x2
+import numpy as np
+grid1 = [
+		[1, 1, 1, 1],
+		[1, 1, 1, 1],
+		[1, 1, 1, 1],
+		[1, 1, 1, 1]]
+
+grid2 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 3],
+		[0, 1, 0, 4],
+		[3, 4, 2, 1]]
+
+grid3 = [
+		[1, 2, 3, 4],
+		[2, 1, 4, 3],
+		[3, 4, 2, 1],
+		[4, 3, 1, 2]]
+
+grid4 = [
+		[1, 3, 4, 2],
+		[4, 2, 1, 3],
+		[2, 1, 3, 4],
+		[3, 4, 2, 1]]
+
+#Grids 4-7 are 3x3
+grid5 = [
+		[1, 2, 3, 4, 5, 6, 7, 8, 9,],
+		[2, 3, 4, 5, 6, 7, 8, 9, 1,],
+		[3, 4, 5, 6, 7, 8, 9, 1, 2,],
+		[4, 5, 6, 7, 8, 9, 1, 2, 3,],
+		[5, 6, 7, 8, 9, 1, 2, 3, 4,],
+		[6, 7, 8, 9, 1, 2, 3, 4, 5,],
+		[7, 8, 9, 1, 2, 3, 4, 5, 6,],
+		[8, 9, 1, 2, 3, 4, 5, 6, 7,],
+		[9, 1, 2, 3, 4, 5, 6, 7, 8,]]
+
+grid6 = [
+		[6, 1, 7, 8, 4, 2, 5, 3, 9,],
+		[7, 4, 5, 3, 6, 9, 1, 8, 2,],
+		[8, 3, 2, 1, 7, 5, 4, 6, 9,],
+		[1, 5, 8, 6, 9, 7, 3, 2, 4,],
+		[9, 6, 4, 2, 3, 1, 8, 7, 5,],
+		[2, 7, 3, 5, 8, 4, 6, 9, 1,],
+		[4, 8, 7, 9, 5, 6, 2, 1, 3,],
+		[3, 9, 1, 4, 2, 8, 7, 5, 6,],
+		[5, 2, 6, 7, 1, 3, 9, 4, 8,]]
+
+grid7 = [
+		[6, 1, 9, 8, 4, 2, 5, 3, 7,],
+		[7, 4, 5, 3, 6, 9, 1, 8, 2,],
+		[8, 3, 2, 1, 7, 5, 4, 6, 9,],
+		[1, 5, 8, 6, 9, 7, 3, 2, 4,],
+		[9, 6, 4, 2, 3, 1, 8, 7, 5,],
+		[2, 7, 3, 5, 8, 4, 6, 9, 1,],
+		[4, 8, 7, 9, 5, 6, 2, 1, 3,],
+		[3, 9, 1, 4, 2, 8, 7, 5, 6,],
+		[5, 2, 6, 7, 1, 3, 9, 4, 8,]]
+
+#grids 8-10 are 2x3
+grid8 = [
+		[0, 0, 6, 0, 0, 3],
+		[5, 0, 0, 0, 0, 0],
+		[0, 1, 3, 4, 0, 0],
+		[0, 0, 0, 0, 0, 6],
+		[0, 0, 1, 0, 0, 0],
+		[0, 5, 0, 0, 6, 4]]
+
+grid9 = [
+		[1, 2, 6, 5, 4, 3],
+		[5, 3, 4, 6, 2, 1],
+		[6, 1, 3, 4, 5, 2],
+		[2, 5, 5, 3, 1, 6],
+		[4, 6, 1, 2, 3, 5],
+		[3, 5, 2, 1, 6, 4]]
+
+grid10 = [
+		[1, 2, 6, 5, 4, 3],
+		[5, 3, 4, 6, 2, 1],
+		[6, 1, 3, 4, 5, 2],
+		[2, 4, 5, 3, 1, 6],
+		[4, 6, 1, 2, 3, 5],
+		[3, 5, 2, 1, 6, 4]]
+
+
+grids = [(grid1, 2, 2), (grid2, 2, 2), (grid3, 2, 2), (grid4, 2, 2), 
+		(grid5, 3, 3), (grid6, 3, 3), (grid7, 3, 3),
+		(grid8, 2, 3), (grid9, 2, 3), (grid10, 2, 3)]
+
+expected_outputs = [False, False, False, True, False, False, True, False, False, True]
+
+'''
+===================================
+DO NOT CHANGE CODE ABOVE THIS LINE
+===================================
+'''
+
+#To complete the first assignment, please write the code for the following function
+def check_solution(grid_input):
+	'''
+	This function is used to check whether a sudoku board has been correctly solved
+
+	args: grid - representation of a suduko board as a nested list.
+	returns: True (correct solution) or False (incorrect solution)
+	'''
+	grid_size = grid_input[1]*grid_input[2]
+	grid = grid_input[0]
+
+	# print(check_rows(grid_input), check_colums(grid_input), check_units(grid_input))
+	if check_rows(grid_input) and check_colums(grid_input) and check_units(grid_input):
+		return True
+	else:
+		return False
+
+def check_rows(grid_input):
+	#check through every row and colum
+	grid = grid_input[0]
+	for rows in grid:
+		if np.size(rows) != np.size(np.unique(rows)):
+			return False
+	return True
+
+def check_colums(grid_input):
+	grid_size = grid_input[1]*grid_input[2]
+	grid = grid_input[0]
+	for colums in range(grid_size):
+		new_row = []
+		for rows in range(grid_size):
+			new_row.append(grid[rows][colums])
+		if np.size(new_row) != np.size(np.unique(new_row)):
+			return False
+	return True
+
+def check_units(grid_input):
+	#check through every unit
+	grid_size = grid_input[1]*grid_input[2]
+	grid = grid_input[0]
+	for units_row_start in range(0,grid_size,grid_input[2]):
+		for units_coloum_start in range (0,grid_size,grid_input[1]):
+			grid = np.array(grid)
+			new_row = grid[units_coloum_start:units_coloum_start+grid_input[1],units_row_start:units_row_start+grid_input[2]]
+			if np.size(new_row) != np.size(np.unique(new_row)):
+				return False
+	return True
+
+
+'''
+===================================
+DO NOT CHANGE CODE BELOW THIS LINE
+===================================
+'''
+def main():
+	'''
+	This function will call the check_solution function on each of the provided grids, 
+	comparing the answer to the expected output. Each correct output is given 10 'points
+	'''
+
+	points = 0
+
+	print("Running test script for coursework 1")
+	print("====================================")
+	
+	#Loop through the grids and expected outputs together
+	for (i, (grid, output)) in enumerate(zip(grids, expected_outputs)):
+		#zip表示把括号内的东西前一个后一个一组打包
+		#enumerate把每一个元组编号
+		#Compare output to expected output
+		print("Checking grid: %d" % (i+1))
+		checker_output = check_solution(grid)
+
+		if checker_output == expected_outputs[i]:
+			#Output is correct - yay!
+			print("grid %d correct" % (i+1))
+			points = points + 5
+		else:
+			#Output is incorrect - print both output and expected output.
+			print("grid %d incorrect" % (i+1))
+			print("Output was: %s, but expected: %s" % (checker_output, expected_outputs[i]))
+
+	print("====================================")
+	print("Test script complete, Total points: %d" % points)
+
+
+if __name__ == "__main__":
+	main()
